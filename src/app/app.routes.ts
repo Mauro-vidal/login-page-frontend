0c5884426d4399d3c5fd62b5ac8a5e5b29ea929c
Mauro-vidal <mauroviidal@gmail.com>
2024-11-20 23:37:20 -0300

    serviço de guarda para usuários autenticados.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { UserComponent } from './pages/user/user.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 export const routes: Routes = [
     {
@@ -18,11 +19,12 @@ export const routes: Routes = [
         component: SignupComponent
     },
     {
-        path: "**",
-        redirectTo: "login" // Redireciona para login em caso de rota inválida
+        path: "user",
+        component: UserComponent,
+        canActivate: [AuthGuard] //rota de guarda
     },
     {
-        path: "user",
-        component: UserComponent
+        path: "**",
+        redirectTo: "login" // Redireciona para login em caso de rota inválida
     },
 ];
