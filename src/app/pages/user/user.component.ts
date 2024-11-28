30f59337b39edd42575517a3a93b025672f72fe3
Mauro-vidal <mauroviidal@gmail.com>
2024-11-27 23:03:07 -0300

    ajustes layout

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../types/user-response';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserServiceService } from '../../services/user-service.service';
 import { CommonModule } from '@angular/common';
 import { ToastrService } from 'ngx-toastr';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -13,15 +15,15 @@ import { ToastrService } from 'ngx-toastr';
     FormsModule
   ],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit{
-
+export class UserComponent implements OnInit {
   users: User[] = [];
-  selectedUser: User | null = null;
-  userForm!: FormGroup;
   filteredUsers: User[] = [];
   paginatedUsers: User[] = [];
+  selectedUser: User | null = null;
+  userForm!: FormGroup;
+
   searchTerm: string = '';
   currentPage: number = 1;
   itemsPerPage: number = 5;
@@ -33,68 +35,68 @@ export class UserComponent implements OnInit{
     private fb: FormBuilder
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.fetchUsers();
     this.initForm();
   }
 
-  fetchUsers() {
+  fetchUsers(): void {
     this.userService.getAllUsers().subscribe((data) => {
       this.users = data;
       this.filteredUsers = data;
       this.updatePagination();
-
     });
   }
 
-  filterUsers() {
+  filterUsers(): void {
     this.filteredUsers = this.users.filter((user) =>
       user.name.toLowerCase().includes(this.searchTerm.toLowerCase())
     );
-    this.currentPage = 1; // Resetar para a primeira página
+    this.currentPage = 1;
     this.updatePagination();
   }
 
-  updatePagination() {
+  updatePagination(): void {
     this.totalPages = Math.ceil(this.filteredUsers.length / this.itemsPerPage);
     const start = (this.currentPage - 1) * this.itemsPerPage;
     const end = start + this.itemsPerPage;
     this.paginatedUsers = this.filteredUsers.slice(start, end);
   }
 
-  changePage(page: number) {
+  changePage(page: number): void {
     this.currentPage = page;
     this.updatePagination();
   }
 
-  initForm() {
+  initForm(): void {
     this.userForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.minLength(6)]],
+      password: ['', [Validators.minLength(6)]]
     });
   }
 
-  editUser(user: User) {
+  editUser(user: User): void {
     this.selectedUser = user;
     this.userForm.patchValue({
       name: user.name,
       email: user.email,
-      password: '',
+      password: ''
     });
   }
 
-  saveUser() {
+  saveUser(): void {
     if (this.selectedUser && this.userForm.valid) {
       const updatedUser = { ...this.selectedUser, ...this.userForm.value };
       this.userService.updateUser(this.selectedUser.id, updatedUser).subscribe(() => {
+        this.toastr.success('Usuário atualizado com sucesso!', 'Sucesso');
         this.fetchUsers();
         this.closeForm();
       });
     }
   }
 
-  confirmDelete(id: string) {
+  confirmDelete(id: string): void {
     this.toastr
       .warning('Tem certeza que deseja excluir?', 'Confirmação', {
         timeOut: 3000,
@@ -104,17 +106,15 @@ export class UserComponent implements OnInit{
       .onTap.subscribe(() => this.deleteUser(id));
   }
 
-  deleteUser(id: string) {
+  deleteUser(id: string): void {
     this.userService.deleteUser(id).subscribe(() => {
       this.toastr.success('Usuário excluído com sucesso!', 'Sucesso');
       this.fetchUsers();
     });
   }
 
-  closeForm() {
+  closeForm(): void {
     this.selectedUser = null;
     this.userForm.reset();
   }
-  
-
 }
