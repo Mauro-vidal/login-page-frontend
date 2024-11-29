1f2ad197ec4c18ebd60b6bd642e16e6deaa826c4
Mauro-vidal <mauroviidal@gmail.com>
2024-11-29 12:37:55 -0300

    Rota de user adicionada no botão

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,47 +1,33 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
-import { UserComponent } from '../../pages/user/user.component';
 
 @Component({
   selector: 'app-default-login-layout',
   standalone: true,
-  imports: [],
   templateUrl: './default-login-layout.component.html',
-  styleUrl: './default-login-layout.component.scss'
+  styleUrls: ['./default-login-layout.component.scss'],
 })
 export class DefaultLoginLayoutComponent {
-
-  @Input() title: string = "";
-  @Input() primaryBtnText: string = "";
-  @Input() secondaryBtnText: string = "";
-  
-  //adicionei isso para user
-  @Input() btnUserTexr: string = "";
-  
+  @Input() title: string = '';
+  @Input() primaryBtnText: string = '';
+  @Input() secondaryBtnText: string = '';
+  @Input() btnUserTexr: string = '';
   @Input() disablePrimaryBtn: boolean = true;
-  @Output("submit") onSubmit = new EventEmitter(); // submit do formulário
 
-  @Output("navigate") onNavigate = new EventEmitter();
-  
-  //submit do formulário
-  submit(){
-    this.onSubmit.emit();
+  @Output() submit = new EventEmitter(); // submit do formulário
+  @Output() navigate = new EventEmitter();
+
+  constructor(private router: Router) {}
+
+  submitForm() {
+    this.submit.emit();
   }
 
-  navigate(){
-    this.onNavigate.emit();
+  navigateSignup() {
+    this.navigate.emit();
   }
 
-  //adicionei isso para user
-  submitUser() {
-    this.onSubmit.emit();
-  }
-
-  //adicionei isso para user
   navigateUser() {
-    this.onNavigate.emit();
+    this.router.navigate(['user']);
   }
-
-
-
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -44,7 +44,7 @@ export class LoginComponent {
     this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
       next: () => this.toastService.success("Login feito com sucesso!"),
       error: () => this.toastService.error("Erro inesperado! Tente novamente mais tarde")
-      
+
     })
   }
 
